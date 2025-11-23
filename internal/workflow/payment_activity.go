0dda076445b72351c067df05b7a334ad516623ad
Ajii <ajiedafuq@gmail.com>
2025-11-23 14:22:50 +0700

    add log payment_activity

--- internal/workflow/payment_activity.go
+++ internal/workflow/payment_activity.go
@@ -3,13 +3,16 @@ package workflow
 import "fmt"
 
 func SendPaymentReminderActivity(orderID string) error {
-	fmt.Println("Push notif: ", orderID)
+	fmt.Println("Push notif reminder: ", orderID)
+
 	// TODO push notif
 
 	return nil
 }
 
 func CheckPaymentStatusActivity(orderID string) (bool, error) {
+	fmt.Println("Check payment status: ", orderID)
+
 	// TODO Query DB or API
 
 	// return true if paid, false if not
@@ -17,6 +20,8 @@ func CheckPaymentStatusActivity(orderID string) (bool, error) {
 }
 
 func CancelOrderActivity(orderID string) error {
+	fmt.Println("Push notif cancel order : ", orderID)
+
 	// TODO update DB: order canceled and push notif
 
 	return nil
